5ed5f0c587a89b002140132f80e7132d35ba660d
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2024-10-09 13:48:20 -0300

    feat: add triangulation to CMake

---
+++ src/triangulation/triangulation.cpp
@@ -0,0 +1,8 @@
+#include "triangulation.hpp"
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+
+    return 0;
+}
--- src/triangulation/triangulation.hpp
+++ src/triangulation/triangulation.hpp
@@ -18,45 +18,16 @@
 #include<opencv2/core/core.hpp>
 #include<opencv2/opencv.hpp>
 
-// initialize values for StereoSGBM parameters
-
-
-
-
-class DisparityNode : public rclcpp::Node
+class TriangulationNode : public rclcpp::Node
 {
     public:
-        DisparityNode(sensor_msgs::msg::CameraInfo infoL, sensor_msgs::msg::CameraInfo infoR);
+        TriangulationNode(sensor_msgs::msg::CameraInfo infoL);
 
     private:
-        using ImageMsg = sensor_msgs::msg::Image;
-        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image> approximate_sync_policy;
-
-        void GrabStereo(const sensor_msgs::msg::Image::ConstSharedPtr msgRGB, const sensor_msgs::msg::Image::ConstSharedPtr msgD);
-        void RectifyImages(cv::Mat imgL, cv::Mat imgR);
-        void CalculateRectificationRemaps();
-        void UpdateParameters(const std_msgs::msg::Int16MultiArray::ConstSharedPtr params_message);
-
-
-        cv::Mat left_map1, left_map2;
-        cv::Mat right_map1, right_map2;
-
-        cv::Mat rectImgL, rectImgR;
-
-        cv_bridge::CvImageConstPtr cv_ptrLeft;
-        cv_bridge::CvImageConstPtr cv_ptrRight;
+        float baseline_x_fx_, principal_x_, principal_y_, fx_, fy_;
 
         sensor_msgs::msg::CameraInfo left_camera_info;
-        sensor_msgs::msg::CameraInfo right_camera_info;
+        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> disparity_sub;
 
-        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> left_sub;
-        std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> right_sub;
-
-        std::shared_ptr<rclcpp::Subscription<std_msgs::msg::Int16MultiArray>> params_sub;
-
-        std::shared_ptr<message_filters::Synchronizer<approximate_sync_policy>> syncApproximate;
-
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr disparity_publisher;
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr rect_left_publisher;
-        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr rect_right_publisher;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_publisher;
 };
